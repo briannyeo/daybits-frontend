47ecababff21109fffdc8c3e8a6dfb3a020b0165
Krystle Lau <krystlelau90@gmail.com>
2022-04-07 19:27:01 +0800

    sign in page added as part of owl carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Owldemo1 from "./Owldemo1";
-import Login from "./Login";
 
 const Home = () => {
   return (
     <div>
       <Owldemo1 />
-      <Login />
     </div>
   );
 };
