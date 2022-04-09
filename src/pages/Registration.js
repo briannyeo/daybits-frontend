c2632312649aaf0ed7013377d7b03c8a83d9b8ac
Krystle Lau <krystlelau90@gmail.com>
2022-04-09 16:43:22 +0800

    add registration page

---
+++ src/pages/Registration.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+
+const Registration = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpw, setConfirmpw] = useState("");
+
+  const handleChangeusername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handleChangepassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleChangeconfirmpw = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setConfirmpw(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password === confirmpw) {
+      const userInfo = { username, password }; //backend
+      console.log(userInfo);
+      //createUser(userInfo); //LINK to backend
+    } else {
+      alert("passwords do not match");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Registration</h1>
+      <Box
+        sx={{ flexGrow: 1 }}
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Grid container spacing={1}></Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Username"
+            multiline
+            maxRows={4}
+            value={username}
+            onChange={handleChangeusername}
+          />
+        </Grid>
+
+        <TextField
+          id="outlined-multiline-static"
+          label="Password"
+          multiline
+          maxRows={4}
+          value={password}
+          onChange={handleChangepassword}
+        />
+
+        <TextField
+          id="outlined-multiline-static"
+          label="Confirm Password"
+          multiline
+          maxRows={4}
+          value={confirmpw}
+          onChange={handleChangeconfirmpw}
+        />
+      </Box>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
+
+export default Registration;
